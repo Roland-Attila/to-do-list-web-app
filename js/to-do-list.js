04f938dde9d810138d0947dc0622468c6af3b551
Roland <rolandattila@yahoo.com>
2019-11-11 20:53:40 +0200

    added support for creating new items

--- js/to-do-list.js
+++ js/to-do-list.js
@@ -1,37 +1,65 @@
-window.ToDoList= {
-  API_URL: "http://localhost:8081/to-do-items",
-  getItems: function () {
-      $.ajax({
-          url: ToDoList.API_URL,
-          method: "GET"
-      }).done(function (response) {
-          console.log("GET done");
-          console.log(response);
+window.ToDoList = {
+    API_URL: "http://localhost:8081/to-do-items",
+    getItems: function () {
+        $.ajax({
+            url: ToDoList.API_URL,
+            method: "GET"
+        }).done(function (response) {
+            console.log("GET done");
+            console.log(response);
 
-          ToDoList.displayItems(JSON.parse(response));
-      });
-  },
+            ToDoList.displayItems(JSON.parse(response));
+        });
+    },
+
+    createItem: function () {
+        let descriptionValue = $("#description-field").val();
+        let deadlineValue = $("#deadline-field").val();
+        var requestBody = {
+            description: descriptionValue,
+            deadline: deadlineValue
+        };
+
+        $.ajax({
+            url: ToDoList.API_URL,
+            method: "POST",
+            //MIME type
+            contentType: "application/json",
+            data: JSON.stringify(requestBody)
+        }).done(function () {
+            ToDoList.getItems();
+        })
+    },
+
     displayItems: function (items) {
-      var tableContent = "";
-      items.forEach(item => tableContent += ToDoList.getItemTableRow(item));
+        var tableContent = "";
+        items.forEach(item => tableContent += ToDoList.getItemTableRow(item));
 
-      console.log(tableContent);
+        console.log(tableContent);
 
-      $("#to-do-items tbody").html (tableContent);
+        $("#to-do-items tbody").html(tableContent);
     },
 
     getItemTableRow: function (item) {
-      //spread
-      var deadline = new Date(...item.deadline).toLocaleDateString("ro");
-      //thernary operator
-      var checkedAttribute = item.done ? "checked" : "";
+        //spread
+        var deadline = new Date(...item.deadline).toLocaleDateString("ro");
+        //thernary operator
+        var checkedAttribute = item.done ? "checked" : "";
         return `<tr>
             <td>${item.description}</td>
             <td>${deadline}</td>
             <td><input type="checkbox" class="mark-done" data-id="${item.id}" ${checkedAttribute}/></td>
             <td><a href="#" class="delete-item" data-id="${item.id}"><i class="far fa-trash-alt"></i></a></td>
         </tr>`
+    },
+
+    bindEvents: function () {
+        $("#create-item-form").submit(function (event) {
+            event.preventDefault();
+            ToDoList.createItem();
+        })
     }
 };
 
 ToDoList.getItems();
+ToDoList.bindEvents();
